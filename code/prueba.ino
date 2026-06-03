27b2ede043f1300f4a2f976de87661fcfc37e48d
Marian Zama <134462487+bynessie@users.noreply.github.com>
2026-06-02 19:26:18 -0600

    Update prueba.ino

--- code/prueba.ino
+++ code/prueba.ino
@@ -1,23 +1,33 @@
 #include "HX711.h"
 
+// Pines del HX711
 const int LOADCELL_DOUT_PIN = 16;
 const int LOADCELL_SCK_PIN  = 4;
 
-HX711 scale;
+HX711 balanza;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.tare();
+  // Inicializar HX711
+  balanza.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 
-  Serial.println("HX711 listo");
+  // Factor de calibración
+  balanza.set_scale(1811.59);
+
+  // Tara (pone el peso actual como cero)
+  balanza.tare();
+
+  Serial.println("Bascula lista");
 }
 
 void loop() {
 
-  Serial.print("Lectura: ");
-  Serial.println(scale.get_units());
+  float peso = balanza.get_units(5);
+
+  Serial.print("Peso: ");
+  Serial.print(peso);   // quita el signo menos si aparece invertido
+  Serial.println(" g");
 
   delay(500);
 }
